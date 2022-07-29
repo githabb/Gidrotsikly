e5ee24a8d14dcc7af3a5f489b5667a6708e18644
Yurii <yurievch@gmail.com>
2022-07-29 22:26:34 +0300

    1 - верстка фильтра

--- js/main.js
+++ js/main.js
@@ -34,9 +34,9 @@ $(function(){
 
     $('.filter-style').styler();
 
-    $('filter__item-drop').on('click', function(){
+    $('.filter__item-drop').on('click', function(){
       $(this).toggleClass('filter__item-drop--active');
-      $(this).next().slideToggle();
+      $(this).next().slideToggle('200');
 
     });
 
