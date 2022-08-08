52c0b5cf90fe34ecec90137567e02f6409f4e916
Yurii <yurievch@gmail.com>
2022-08-08 17:10:58 +0300

    бургер меню

--- js/main.js
+++ js/main.js
@@ -64,6 +64,10 @@ $(function(){
       normalFill: "#C4C4C4"
     });
 
+    $('.menu__btn').on('click', function(){
+      $('.menu-mobile__list').toggleClass('menu-mobile__list--active')
+    });
+
 });
 
 
