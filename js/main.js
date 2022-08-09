1fe2de8d472004c25e1a044d283100729d87e1dd
Yurii <yurievch@gmail.com>
2022-08-09 20:09:05 +0300

    баннер

--- js/main.js
+++ js/main.js
@@ -39,6 +39,33 @@ $(function(){
       '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt="arrow-left"></button>',
        nextArrow:
       '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt="arrow-right"></button>',
+      responsive:[
+        {
+          breakpoint: 1301,
+          settings:{
+            arrows: false,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 1201,
+          settings:{
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 870,
+          settings:{
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 590,
+          settings:{
+            slidesToShow: 1,
+          }
+        }
+      ]  
     });
 
     $('.filter-style').styler();
