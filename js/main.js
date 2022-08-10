b2674c86200b239cb0b46bf5a81e6427cfa2d63d
Yurii <yurievch@gmail.com>
2022-08-10 19:41:35 +0300

    правка баг

--- js/main.js
+++ js/main.js
@@ -20,10 +20,14 @@ $(function(){
       e.preventDefault();
 
       $($(this).siblings()).removeClass('tab--active');
-      $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+      $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
 
       $(this).addClass('tab--active');
       $($(this).attr('href')).addClass('tabs-content--active');
+
+      $('.product-slider').slick('setPosition');
+
+      
 
     });
 
